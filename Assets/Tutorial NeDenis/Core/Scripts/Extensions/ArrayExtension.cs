3dd9af3d3a3f07b9f7a09de4103e86e79348aaa8
Denis Shatskikh <84654874+DShatskikh@users.noreply.github.com>
2024-05-12 03:30:40 +0700

    Update ArrayExtension.cs

--- Assets/Tutorial NeDenis/Core/Scripts/Extensions/ArrayExtension.cs
+++ Assets/Tutorial NeDenis/Core/Scripts/Extensions/ArrayExtension.cs
@@ -4,6 +4,7 @@ using Random = UnityEngine.Random;
 
 namespace Core
 {
+    //Расширение для массива
     public static class ArrayExtension
     {
         public static T GetRandomItem<T>(this T[] array)
